4f52411d0bd92226fe490807f1dc285e24a12599
gianella <gia.ne12@hotmail.com>
2024-06-21 19:29:27 -0500

    Initial commit

---
+++ io/src/lib.rs
@@ -0,0 +1,123 @@
+#![no_std]
+
+use gmeta::{In, InOut, Metadata, Out};
+use gstd::{prelude::*, ActorId};
+
+pub struct ProgramMetadata;
+
+// 1. Define actions, events, errors and state for your metadata.
+impl Metadata for ProgramMetadata {
+    type Init = In<InitStruct>;
+    type Handle = InOut<ConcertActions, Result<ConcertEvents, ConcertErrors>>;
+    type Others = ();
+    type Reply = ();
+    type Signal = ();
+    type State = InOut<ConcertQuery, ConcertQueryReply>;
+}
+
+// 2. Create your init Struct(Optional)
+#[derive(Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct InitStruct {
+    // Example:
+    pub ft_program_id: ActorId,
+}
+
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct CreateConcertProps {
+    pub creator: ActorId,
+    pub name: String,
+    pub description: String,
+    pub number_of_tickets: u128,
+    pub token_id: u128,
+}
+
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct BuyTicketsProps {
+    pub amount: u128,
+}
+
+// 3. Create your own Actions
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum ConcertActions {
+    // Actions
+    Create(CreateConcertProps),
+    Hold,
+    BuyTickets(BuyTicketsProps),
+}
+
+// Add Your Main State
+#[derive(Debug, Decode, Encode, Clone, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct ConcertState {
+    pub name: String,
+    pub description: String,
+    pub number_of_tickets: u128,
+    pub creator: ActorId,
+    pub owner_id: ActorId,
+}
+
+// 4. Create your own Events
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum ConcertEvents {
+    // Events
+    Creation {
+        creator: ActorId,
+        concert_id: u128,
+        number_of_tickets: u128,
+    },
+    Hold {
+        concert_id: u128,
+    },
+    Purchase {
+        concert_id: u128,
+        amount: u128,
+    },
+}
+
+// 5. Create your own Errors
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum ConcertErrors {}
+
+// 6. Create your own Struct
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct ConcertCustomFields {
+    pub name: String,
+    pub description: String,
+}
+
+// 7. Create your own enum
+#[derive(Debug, Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum CustomStates {
+    // Add your states
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum ConcertQuery {
+    ReplyDefault,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum ConcertQueryReply {
+    ReplyDefault(String),
+}
---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+#![no_std]
+use gstd::{async_main, collections::HashMap, msg, prelude::*, ActorId};
+use io::*;
+
+// 1. Create the main state as a static variable.
+static mut STATE: Option<Concert> = None;
+
+// Create a public State
+#[derive(Clone, Default)]
+pub struct Concert {
+    pub name: String,
+    pub description: String,
+    pub number_of_tickets: u128,
+    pub creator: ActorId,
+    pub owner_id: ActorId,
+    pub concert_id: u128,
+}
+
+// Create a implementation on State
+impl Concert {
+    async fn create_concert(
+        &mut self,
+        input: CreateConcertProps,
+    ) -> Result<ConcertEvents, ConcertErrors> {
+        // Add Validations
+
+        // Add your logic for example
+
+        // Change State
+        self.name = input.name;
+
+        Ok(ConcertEvents::Creation {
+            creator: self.creator,
+            concert_id: self.concert_id,
+            number_of_tickets: self.number_of_tickets,
+        })
+    }
+
+    async fn hold_concert(&mut self) -> Result<ConcertEvents, ConcertErrors> {
+        // Add Validations
+
+        // Add your logic for example
+
+        // Change State
+
+        Ok(ConcertEvents::Hold {
+            concert_id: self.concert_id,
+        })
+    }
+    async fn buy_tickets(
+        &mut self,
+        input: BuyTicketsProps,
+    ) -> Result<ConcertEvents, ConcertErrors> {
+        // Add Validations
+
+        // Add your logic for example
+
+        // Change State
+        self.number_of_tickets = input.amount;
+
+        Ok(ConcertEvents::Purchase {
+            concert_id: self.concert_id,
+            amount: self.number_of_tickets,
+        })
+    }
+}
+
+// 3. Create the init() function of your contract.
+#[no_mangle]
+extern "C" fn init() {
+    let state = Concert {
+        ..Default::default()
+    };
+
+    unsafe { STATE = Some(state) };
+}
+
+// 4.Create the main() function of your contract.
+#[async_main]
+async fn main() {
+    // We load the input message
+    let action: ConcertActions = msg::load().expect("Could not load Action");
+
+    let state = unsafe { STATE.as_mut().expect("The contract is not initialized") };
+
+    // We receive an action from the user and update the state. Example:
+
+    let reply = match action {
+        ConcertActions::Create(input) => state.create_concert(input).await,
+        ConcertActions::Hold => state.hold_concert().await,
+        ConcertActions::BuyTickets(input) => state.buy_tickets(input).await,
+    };
+    msg::reply(reply, 0).expect("Error in sending a reply");
+}
+
+// 5. Create the state() function of your contract.
+#[no_mangle]
+extern "C" fn state() {
+    let state = unsafe { STATE.take().expect("Unexpected error in taking state") };
+    let query: ConcertQuery = msg::load().expect("Unable to load the state query");
+    let reply = match query {
+        ConcertQuery::ReplyDefault => ConcertQueryReply::ReplyDefault(state.name),
+    };
+    msg::reply(reply, 0).expect("Error on state");
+}
